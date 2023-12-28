7eb8d553f7e1a74352345cddceb52dd525a8a8d7
Edgar Jesus <mendesbr92@gmail.com>
2023-12-27 21:29:57 -0300

    feat: algo pratico com argumentos

---
+++ nodejs/2-fundamentos/6-pratica-com-args/index.js
@@ -0,0 +1,28 @@
+const minimist = require('minimist');
+
+// externo
+const soma = require('./soma').soma;
+
+const args = minimist(process.argv.slice(2));
+
+
+const a = parseInt(args['a']);
+const b = parseInt(args['b']);
+
+console.log(a + b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
