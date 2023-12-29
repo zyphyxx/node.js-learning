c2b4cd29d3d9b882ca1a4f764e203376304dd8cc
Edgar Jesus <mendesbr92@gmail.com>
2023-12-28 21:46:49 -0300

    feat: sync e async

---
+++ nodejs/2-fundamentos/13-sync-async/assincrono.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+console.log('inicio');
+
+fs.writeFile('arquivo.txt', 'oi async', function (err) {
+    setTimeout(function () {
+        console.log('Arquivo criado!');
+    }, 1000);
+});
+
+console.log('Fim');
+
