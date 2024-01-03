fb3b6ce6a562bd9354e57a473213d3799fd0f1a1
Edgar Jesus <mendesbr92@gmail.com>
2024-01-02 21:31:05 -0300

    feat: inicialização do sistema

--- nodejs/05-ACCOUNTS/index.js
+++ nodejs/05-ACCOUNTS/index.js
@@ -1,4 +1,7 @@
 // modulos externos
+// ver. npm install inquirer@8.1.2
+// ver. npm install chalk@4.1.2
+
 const inquirer = require('inquirer');
 const chalk = require('chalk');
 
@@ -6,3 +9,25 @@ const chalk = require('chalk');
 const fs = require('fs');
 
 console.log('Iniciamos o Accounts');
+
+operation();
+
+function operation() {
+
+  inquirer.prompt([{
+    type: 'list',
+    name: 'action',
+    message: 'O que você deseja fazer ?',
+    choices: [
+      'Criar conta',
+      'Consultar Saldo',
+      'Depositar',
+      'Sacar',
+      'Sair'
+    ]
+  }]).then((answer) =>{
+    const action = answer['action'];
+    console.log(action);
+  })
+  .catch((err) => console.log(err));
+}
