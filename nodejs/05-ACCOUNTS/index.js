b5053b4609da29fd46de0dc466e765b9275d7a85
Edgar Jesus <mendesbr92@gmail.com>
2024-01-04 19:42:17 -0300

    feat: finalizando criação de conta

--- nodejs/05-ACCOUNTS/index.js
+++ nodejs/05-ACCOUNTS/index.js
@@ -41,4 +41,45 @@ function operation() {
 function createAccount () {
   console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'));
   console.log(chalk.green('Defina as opções da sua conta a seguir'));
+
+  buildAccount();
 }
+
+function buildAccount() {
+  inquirer
+    .prompt([
+      {
+        name: 'accountName',
+        message: 'Digite um nome para a sua conta:',
+      },
+    ])
+    .then((answer) => {
+      console.info(answer['accountName'])
+
+      const accountName = answer['accountName']
+
+      if (!fs.existsSync('accounts')) {
+        fs.mkdirSync('accounts')
+      }
+
+      if (fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(
+          chalk.bgRed.black('Esta conta já existe, escolha outro nome!'),
+        )
+        buildAccount(accountName)
+      }
+
+      fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        '{"balance":0}',
+        function (err) {
+          console.log(err)
+        },
+      )
+
+      console.log(chalk.green('Parabéns, sua conta foi criada!'))
+      operation()
+    })
+    
+};
+
