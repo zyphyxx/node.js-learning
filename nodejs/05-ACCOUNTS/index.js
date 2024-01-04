b9a3f69bf69c8c087b8ed5f74a160758f428f229
Edgar Jesus <mendesbr92@gmail.com>
2024-01-03 21:45:54 -0300

    feat: criação de conta

--- nodejs/05-ACCOUNTS/index.js
+++ nodejs/05-ACCOUNTS/index.js
@@ -19,7 +19,7 @@ function operation() {
     name: 'action',
     message: 'O que você deseja fazer ?',
     choices: [
-      'Criar conta',
+      'Criar Conta',
       'Consultar Saldo',
       'Depositar',
       'Sacar',
@@ -27,7 +27,18 @@ function operation() {
     ]
   }]).then((answer) =>{
     const action = answer['action'];
-    console.log(action);
+
+    if(action === 'Criar Conta') {
+      createAccount();
+    }
   })
   .catch((err) => console.log(err));
+
+}
+
+// criar a conta
+
+function createAccount () {
+  console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'));
+  console.log(chalk.green('Defina as opções da sua conta a seguir'));
 }
