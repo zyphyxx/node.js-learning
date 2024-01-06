822c3fdd2be12662fa8e49b16fba69e736eae360
Edgar Jesus <mendesbr92@gmail.com>
2024-01-06 20:17:25 -0300

    feat: retornando html

---
+++ nodejs/03-core-modules/02-retornando-html/index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const port = 3000;
+
+const server = http.createServer((req,res) => {
+
+  res.statusCode = 200;
+  res.setHeader('Contenty-Type', 'text/html');
+  res.end('<h1> Olá, este é meu primeiro server com HTML!</h1>');
+
+});
+
+server.listen(port, () => {
+  console.log(`servidor rodando na porta: ${port}`);
+});
+
