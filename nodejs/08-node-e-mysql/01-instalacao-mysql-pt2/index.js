fdec943973ec1f1cf8293b29e208f632c5ce7b8a
Edgar Jesus <mendesbr92@gmail.com>
2024-01-13 18:55:14 -0300

    feat: treinando e relembrando

--- nodejs/08-node-e-mysql/01-instalacao-mysql-pt2/index.js
+++ nodejs/08-node-e-mysql/01-instalacao-mysql-pt2/index.js
@@ -1,16 +1,49 @@
 const express = require('express');
-const exphds = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const mysql = require('mysql');
 
 const app = express();
 
-app.engine('handlebars', exphds.engine());
+app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
+app.use(express.urlencoded({ extended: true })); // Middleware para analisar dados do corpo da requisição
 
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'springstudent',
+  password: 'springstudent',
+  database: 'tb_dia'
+});
+
+app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.listen(3000);
+app.post('/adicionar-dia', (req, res) => {
+  const x = req.body.dia;
+  const sql = `INSERT INTO meusdias (dia) VALUES ('${x}')`;
 
+  conn.query(sql, (err, result) => {
+    if (err) {
+      console.error('Erro ao inserir registro:', err);
+      res.status(500).send('Erro interno no servidor');
+      return;
+    }
+    
+    console.log('Registro inserido com sucesso!');
+    res.redirect('/');
+  });
+});
+
+conn.connect((err) => {
+  if (err) {
+    console.error('Falha ao conectar com o MySQL:', err);
+    return;
+  }
+
+  console.log('Conectado ao MySQL');
+  app.listen(3000, () => {
+    console.log('Servidor escutando na porta 3000');
+  });
+});
